629e7d0437ddbdadf592bab2ef0f1205f8237419
Harshit Singh <harshit.singh817775@gmail.com>
2024-11-30 22:42:21 +0530

    chore: version update

--- internal/root.go
+++ internal/root.go
@@ -50,7 +50,7 @@ func NewRoot() (*Root, error) {
 func (r *Root) RootCMD() *cobra.Command {
 	rootCMD := &cobra.Command{
 		Use:     "gopush",
-		Version: "1.1.2",
+		Version: "1.1.3",
 		Short: heredoc.Doc(`
 
 			 ██████╗   ██████╗  ██████╗  ██╗   ██╗ ███████╗ ██╗  ██╗
